650a50f4a716cbf8decbf67e45d469e827e1a3e3
ChanwooAn <acwoo97@gmail.com>
2023-07-04 17:53:52 +0900

    [feat] : card 출력 기능 구현
    동물 종류 3가지, 1~12의 총 36가지 카드를 생성하고 print하는 함수를 구현하였음.

--- app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
+++ app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
@@ -2,7 +2,11 @@ package hd.softeer.luckycardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import hd.softeer.luckycardgame.databinding.ActivityMainBinding
+import hd.softeer.luckycardgame.model.Animal
+import hd.softeer.luckycardgame.model.Card
+import hd.softeer.luckycardgame.model.CardNumber
 
 class MainActivity : AppCompatActivity() {
     private var _binding:ActivityMainBinding?=null
@@ -11,7 +15,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        createCardList()
+        printCardList()
     }
+
+    private lateinit var cardList:MutableList<Card>
+    private fun createCardList(){
+        cardList= mutableListOf()
+        for(type in Animal.values()){
+            for(number in 1..12){
+                cardList.add(Card(type, CardNumber(number)))
+            }
+        }
+        cardList.shuffle()
+    }
+    private fun printCardList(){
+        val cardListString= mutableListOf<String>()
+        for(card in cardList){
+            cardListString.add(card.getCardInfo())
+        }
+        Log.d(TAG,cardListString.joinToString(", "))
+    }
+
+
+    companion object{
+        private const val TAG="Main Activity"
+    }
+
 
 
     override fun onDestroy() {
