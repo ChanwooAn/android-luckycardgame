bc760b0a4273ef42582ed89465a2b198794a02fe
ChanwooAn <acwoo97@gmail.com>
2023-07-04 17:51:21 +0900

    [feat] : Animal class 생성
    card 를 생성할 때 전달할 animal type 을 정의하는 enum class 를 생성함.

---
+++ app/src/main/java/hd/softeer/luckycardgame/model/Animal.kt
@@ -0,0 +1,11 @@
+package hd.softeer.luckycardgame.model
+
+enum class Animal(val emoji: String) {
+    Dog("🐶"),
+    Cat("🐱"),
+    Cow("🐮")
+}
+/*
+ animal type은 3개로 국한되므로 예외방지를 위해 sealed class 나 enum class 를 고려할 수 있습니다.
+ 일반적으로 sealed class를 많이 사용하지만 전체 카드를 생성하는 과정에서 iterate를 해야할 것 같아 enum class를 사용하였습니다.
+ */
