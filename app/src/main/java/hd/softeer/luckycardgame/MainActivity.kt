9f0730ffbc15b14c4aedf9f461d722bbf1c5e3ad
ChanwooAn <acwoo97@gmail.com>
2023-07-04 15:21:20 +0900

    [feat] : card board main 화면 구성. - constraint layout 을 사용하여 전체화면 구성. - 모든 view의 width는 화면 가로의 90%를 차지하도록 지정. - card section의 높이는 비율로 지정하여 같은 높이를 갖게 함과 동시에, 화면크기가 달라져도 비슷한 유저경험 제공. 비율지정을 통해 가장하단의 영역이 무조건 card section보다 큰 것을 보장.

--- app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
+++ app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
@@ -2,10 +2,20 @@ package hd.softeer.luckycardgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hd.softeer.luckycardgame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding:ActivityMainBinding?=null
+    private val binding get()=_binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+
+    override fun onDestroy() {
+        _binding=null
+        super.onDestroy()
     }
 }
