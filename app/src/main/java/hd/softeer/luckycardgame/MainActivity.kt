cf2485bbfdaf65cac5421ef33fc750938d242dc0
ChanwooAn <acwoo97@gmail.com>
2023-07-06 11:30:22 +0900

    [feat] : card size logic, card division login update
    카드를 사이즈를 화면크기에 맞게 유동적으로 변경하기 위해, size를 측정하는 logic과 카드를 나누어 주는 logic을 작성함.

--- app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
+++ app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
@@ -3,6 +3,11 @@ package hd.softeer.luckycardgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.ViewTreeObserver
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import hd.softeer.luckycardgame.databinding.ActivityMainBinding
 import hd.softeer.luckycardgame.model.Animal
 import hd.softeer.luckycardgame.model.Card
@@ -13,36 +18,60 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainActivityViewModel::class.java]
+    }
+    private lateinit var card_section_tv_list: List<TextView> //iteration을 위해 각 카드 영역의 textview와 recycler view를 list로 관리.
+    private lateinit var card_section_rv_list: List<RecyclerView>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        createCardList()
-        printCardList()
+        card_section_rv_list = listOf(
+            binding.rvCardSectionA,
+            binding.rvCardSectionB,
+            binding.rvCardSectionC,
+            binding.rvCardSectionD,
+            binding.rvCardSectionE
+        )
+
+
+
+        card_section_tv_list = listOf(
+            binding.tvCardSectionA,
+            binding.tvCardSectionB,
+            binding.tvCardSectionC,
+            binding.tvCardSectionD,
+            binding.tvCardSectionE
+        )
+
+        observeCardSize()
+
     }
 
-    private lateinit var cardList:MutableList<Card>
-    private fun createCardList(){
-        cardList= mutableListOf()
-        for(type in Animal.values()){
-            for(number in 1..12){
-                cardList.add(Card(type, CardNumber(number)))
+
+    /**
+     * card가 담기는 영역의 크기인 textView의 size를 관찰하여 적절하게 카드 size가 설정될 수 있도록 한다.
+     */
+    private fun observeCardSize() {
+        binding.tvCardSectionA.viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                val scale = resources.displayMetrics.density // Get the screen density scale
+                viewModel.setCardWidth(scale, binding.tvCardSectionA.width)
+                viewModel.setCardHeight(scale, binding.tvCardSectionA.height)
+
+
+                binding.tvCardSectionA.viewTreeObserver.removeOnGlobalLayoutListener(this)
             }
-        }
-        cardList.shuffle()
-    }
-    private fun printCardList(){
-        val cardListString= mutableListOf<String>()
-        for(card in cardList){
-            cardListString.add(card.getCardInfo())
-        }
-        Log.d(TAG,cardListString.joinToString(", "))
+        })
     }
 
 
-    companion object{
-        private const val TAG="Main Activity"
+    companion object {
+        private const val TAG = "Main Activity"
 
     }
 }
---
+++ app/src/main/java/hd/softeer/luckycardgame/MainActivityViewModel.kt
@@ -0,0 +1,81 @@
+package hd.softeer.luckycardgame
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import hd.softeer.luckycardgame.model.Animal
+import hd.softeer.luckycardgame.model.Card
+import hd.softeer.luckycardgame.model.CardNumber
+
+class MainActivityViewModel: ViewModel() {
+    private val totalCardList: MutableList<Card> = mutableListOf()
+
+    private val _playersCardList = MutableLiveData<MutableList<List<Card>>>()
+    val playersCardList: LiveData<MutableList<List<Card>>> get()=_playersCardList
+
+    private val _sharedCardList = MutableLiveData<MutableList<Card>>()
+    val sharedCardList:LiveData<MutableList<Card>> get()=_sharedCardList
+
+
+    private var _cardWidth = 0
+    val cardWidth get()=_cardWidth
+
+    private var _cardHeight=0
+    val cardHeight get()=_cardHeight
+
+    init{
+        _playersCardList.value= mutableListOf()
+        _sharedCardList.value= mutableListOf()
+    }
+
+    fun initCardList(playerNum: Int) {
+        totalCardList.clear()
+        makeCardRandom(getMaxCardNum(playerNum))
+        divideCardToPlayers(playerNum)
+    }
+
+    fun setCardWidth(scale:Float,parentPixelWidth:Int){
+        val parentDpWidth = (parentPixelWidth/ scale + 0.5f).toInt() // Convert pixels to dp
+        val cardDpWidth=parentDpWidth/6-8
+        val cardPixelWidth = (cardDpWidth * scale + 0.5f).toInt() // Convert dp to pixels
+
+        _cardWidth=cardPixelWidth
+    }
+    fun setCardHeight(scale:Float,parentPixelHeight:Int){
+        val parentDpHeight = (parentPixelHeight/ scale + 0.5f).toInt()
+        val cardDpHeight=parentDpHeight-16
+        val cardPixelHeight = (cardDpHeight * scale + 0.5f).toInt()
+
+        _cardHeight=cardPixelHeight
+    }
+
+    private fun makeCardRandom(cardMaxNumber: Int) {
+        for (type in Animal.values()) {
+            for (number in 1..cardMaxNumber) {
+                totalCardList.add(Card(type, CardNumber(number)))
+            }
+        }
+        totalCardList.shuffle()
+    }
+
+    private fun divideCardToPlayers(playerNum:Int){
+        val cardsNumPerPlayer=11-playerNum
+        _playersCardList.value!!.clear()
+        _sharedCardList.value!!.clear()
+
+        var start=0
+        var end=cardsNumPerPlayer-1
+        for(i in 0 until playerNum){
+            _playersCardList.value!!.add((totalCardList.slice(start..end)))
+            start+=cardsNumPerPlayer
+            end+=cardsNumPerPlayer
+        }
+
+        _sharedCardList.value!!.addAll(totalCardList.slice(start..totalCardList.lastIndex))
+
+    }
+
+    private fun getMaxCardNum(playerNum: Int): Int {
+        return if (playerNum == 3) 11 else 12
+    }
+}
