863c34397ed05f56cef77d6e9e988c84efb050d4
ChanwooAn <acwoo97@gmail.com>
2023-07-06 11:38:37 +0900

    [feat] : card divide view update logic
    - segmented button을 눌러 player 수를 설정했을 때, card가 분배되고, 분배된 카드에 따라 view를 update하는 logic을 작성함.
    - viewmodel의 값들을 observe하여 view를 update하도록 함
    - recycler view의 adpater class를 생성하였음, 카드는 앞면 뒷면의 두가지 view holder type을 가질 수 있도록 하고, 0번 player만 앞면으로 표기하도록 하였음.
    - ItemDecorator를 사용하여 card 사이 간격을 조정함.

--- app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
+++ app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
@@ -3,9 +3,11 @@ package hd.softeer.luckycardgame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.view.ViewTreeObserver
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import hd.softeer.luckycardgame.databinding.ActivityMainBinding
@@ -48,8 +50,154 @@ class MainActivity : AppCompatActivity() {
         )
 
         observeCardSize()
+        setPlayerNumberButtonListener()
 
     }
+
+    /**
+     * segmented button을 눌렀을 때 player 수를 setting 하고 카드를 초기화하여 나눠주는 등의 작업을 수행한다.
+     */
+    private fun setPlayerNumberButtonListener(){
+        binding.segbtnSelectNumOfPlayer.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            removeCardListObserver()
+            when(checkedId){
+                R.id.bt_player_three->{
+                    viewModel.initCardList(3)
+                    initViewByPlayerNum(3)
+                }
+                R.id.bt_player_four->{
+                    viewModel.initCardList(4)
+                    initViewByPlayerNum(4)
+                }
+                R.id.bt_player_five->{
+                    viewModel.initCardList(5)
+                    initViewByPlayerNum(5)
+                }
+            }
+            observeCardList()
+        }//각 버튼에 맞는 인원수를 viewmodel에 전달하여 카드 list를 적절히 초기화하고, observe하여 view를 update한다.
+    }
+    private fun removeCardListObserver(){
+        viewModel.playersCardList.removeObservers(this)
+    }
+    private fun observeCardList(){
+        val playerCardList=viewModel.playersCardList.value!!
+        viewModel.playersCardList.observe(this){
+            for(playerNum in playerCardList.indices){
+                if(playerCardList[playerNum].isNotEmpty()){
+                    card_section_tv_list[playerNum].text=""
+                }else{
+                    card_section_tv_list[playerNum].text=getCardSectionText(playerNum)
+                }//card list가 비었을 때만 textView에서 text표시
+                setCardRecyclerView(playerNum)
+            }
+        }
+        viewModel.sharedCardList.observe(this){
+            setSharedCardRecyclerView(playerCardList.size)
+        }
+    }
+    private fun getCardSectionText(playerNum:Int):String{
+        return when(playerNum){
+            0->{
+                getString(R.string.card_section_A_text)
+            }
+            1->{
+                getString(R.string.card_section_B_text)
+            }
+            2->{
+                getString(R.string.card_section_C_text)
+            }
+            3->{
+                getString(R.string.card_section_D_text)
+            }
+            4->{
+                getString(R.string.card_section_E_text)
+            }
+            else->{
+                ""
+            }
+        }
+    }
+    private fun initCardRecyclerView(){
+        card_section_rv_list.forEach {
+            it.layoutManager=LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL,false)
+            it.addItemDecoration(PlayerCardItemDecorator(viewModel.cardWidth))
+        }
+    }//player의 카드영역은 player의 수에 따라 바뀌는게 없으므로 따로 빼주어 최초 한 번만 layoutmanager와 item decorator 를 설정한다.
+
+    private fun setCardRecyclerView(playerNum:Int){
+        card_section_rv_list[playerNum].apply {
+            val cardAdapter=CardSectionAdapter(viewModel.playersCardList.value!![playerNum],viewModel.cardWidth,viewModel.cardHeight,playerNum)
+            adapter=cardAdapter
+            layoutManager=LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL,false)
+        }
+
+    }
+    private fun setSharedCardRecyclerView(playerNum: Int){
+        val gridSpan=when(playerNum){
+            4->{
+                4
+            }
+            5->{
+                6
+            }
+            else->{
+                5
+            }
+        }
+
+        binding.rvCardShared.apply {
+            val cardAdapter=CardSectionAdapter(viewModel.sharedCardList.value!!,viewModel.cardWidth,viewModel.cardHeight,3)
+            adapter=cardAdapter
+            layoutManager= GridLayoutManager(this@MainActivity,gridSpan)
+            for(i in 0 until itemDecorationCount){
+                val decoration = getItemDecorationAt(i)
+                if (decoration is SharedCardItemDecorator) {
+                    removeItemDecoration(decoration)
+                }
+            }
+            addItemDecoration(SharedCardItemDecorator(gridSpan))
+        }
+    }
+
+    /**
+     * 인원 수에 맞게 card 영역의 visibility를 적절히 초기화한다.
+     */
+    private fun initViewByPlayerNum(playerNum:Int){
+        when(playerNum){
+            3->{
+                for(i in 0 until 3){
+                    card_section_rv_list[i].visibility= View.VISIBLE
+                    card_section_tv_list[i].visibility=View.VISIBLE
+                }
+
+                card_section_tv_list[3].visibility=View.INVISIBLE
+                card_section_rv_list[3].visibility=View.INVISIBLE
+
+                card_section_tv_list[4].visibility=View.GONE
+                card_section_rv_list[4].visibility=View.GONE
+            }
+
+            4->{
+                for(i in 0 until 4){
+                    card_section_rv_list[i].visibility=View.VISIBLE
+                    card_section_tv_list[i].visibility=View.VISIBLE
+                }
+                card_section_tv_list[4].visibility= View.GONE
+                card_section_rv_list[4].visibility= View.GONE
+            }
+
+            5->{
+                card_section_rv_list.forEach{
+                    it.visibility=View.VISIBLE
+                }
+                card_section_tv_list.forEach{
+                    it.visibility=View.VISIBLE
+                }
+            }
+        }
+    }
+
 
 
     /**
@@ -63,6 +211,7 @@ class MainActivity : AppCompatActivity() {
                 viewModel.setCardWidth(scale, binding.tvCardSectionA.width)
                 viewModel.setCardHeight(scale, binding.tvCardSectionA.height)
 
+                initCardRecyclerView()
 
                 binding.tvCardSectionA.viewTreeObserver.removeOnGlobalLayoutListener(this)
             }
