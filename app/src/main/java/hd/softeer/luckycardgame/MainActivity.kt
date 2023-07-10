dc0ce1d9202ba4d8122e962ccbe59959553d562d
ChanwooAn <acwoo97@gmail.com>
2023-07-10 11:34:55 +0900

    [feat] : MainActivity와 MainActivityViewModel 에 LuckyGame class적용
    기존에 게임관련 logic들은 luckygame 에 모두 선언되어있으므로, LuckyGame class를 활용하는 code로 변경.

--- app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
+++ app/src/main/java/hd/softeer/luckycardgame/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private val viewModel by lazy {
         ViewModelProvider(this)[MainActivityViewModel::class.java]
     }
+
     private lateinit var card_section_tv_list: List<TextView> //iteration을 위해 각 카드 영역의 textview와 recycler view를 list로 관리.
     private lateinit var card_section_rv_list: List<RecyclerView>
 
@@ -57,100 +58,125 @@ class MainActivity : AppCompatActivity() {
     /**
      * segmented button을 눌렀을 때 player 수를 setting 하고 카드를 초기화하여 나눠주는 등의 작업을 수행한다.
      */
-    private fun setPlayerNumberButtonListener(){
+    private fun setPlayerNumberButtonListener() {
         binding.segbtnSelectNumOfPlayer.addOnButtonCheckedListener { group, checkedId, isChecked ->
             removeCardListObserver()
-            when(checkedId){
-                R.id.bt_player_three->{
-                    viewModel.initCardList(3)
+            when (checkedId) {
+                R.id.bt_player_three -> {
+                    viewModel.startGame(3)
                     initViewByPlayerNum(3)
                 }
-                R.id.bt_player_four->{
-                    viewModel.initCardList(4)
+
+                R.id.bt_player_four -> {
+                    viewModel.startGame(4)
                     initViewByPlayerNum(4)
                 }
-                R.id.bt_player_five->{
-                    viewModel.initCardList(5)
+
+                R.id.bt_player_five -> {
+                    viewModel.startGame(5)
                     initViewByPlayerNum(5)
                 }
             }
             observeCardList()
         }//각 버튼에 맞는 인원수를 viewmodel에 전달하여 카드 list를 적절히 초기화하고, observe하여 view를 update한다.
     }
-    private fun removeCardListObserver(){
-        viewModel.playersCardList.removeObservers(this)
+
+    private fun removeCardListObserver() {
+        viewModel.playersList.removeObservers(this)
     }
-    private fun observeCardList(){
-        val playerCardList=viewModel.playersCardList.value!!
-        viewModel.playersCardList.observe(this){
-            for(playerNum in playerCardList.indices){
-                if(playerCardList[playerNum].cardList.isNotEmpty()){
-                    card_section_tv_list[playerNum].text=""
-                }else{
-                    card_section_tv_list[playerNum].text=getCardSectionText(playerNum)
+
+    private fun observeCardList() {
+        val playerCardList = viewModel.playersList.value!!
+        viewModel.playersList.observe(this) {
+            for (playerNum in playerCardList.indices) {
+                if (playerCardList[playerNum].cardList.isNotEmpty()) {
+                    card_section_tv_list[playerNum].text = ""
+                } else {
+                    card_section_tv_list[playerNum].text = getCardSectionText(playerNum)
                 }//card list가 비었을 때만 textView에서 text표시
                 setCardRecyclerView(playerNum)
             }
         }
-        viewModel.sharedCardList.observe(this){
+        viewModel.sharedCardList.observe(this) {
             setSharedCardRecyclerView(playerCardList.size)
         }
     }
-    private fun getCardSectionText(playerNum:Int):String{
-        return when(playerNum){
-            0->{
+
+    private fun getCardSectionText(playerNum: Int): String {
+        return when (playerNum) {
+            0 -> {
                 getString(R.string.card_section_A_text)
             }
-            1->{
+
+            1 -> {
                 getString(R.string.card_section_B_text)
             }
-            2->{
+
+            2 -> {
                 getString(R.string.card_section_C_text)
             }
-            3->{
+
+            3 -> {
                 getString(R.string.card_section_D_text)
             }
-            4->{
+
+            4 -> {
                 getString(R.string.card_section_E_text)
             }
-            else->{
+
+            else -> {
                 ""
             }
         }
     }
-    private fun initCardRecyclerView(){
+
+    private fun initCardRecyclerView() {
         card_section_rv_list.forEach {
-            it.layoutManager=LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL,false)
+            it.layoutManager =
+                LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
             it.addItemDecoration(PlayerCardItemDecorator(viewModel.cardWidth))
         }
     }//player의 카드영역은 player의 수에 따라 바뀌는게 없으므로 따로 빼주어 최초 한 번만 layoutmanager와 item decorator 를 설정한다.
 
-    private fun setCardRecyclerView(playerNum:Int){
+    private fun setCardRecyclerView(playerNum: Int) {
         card_section_rv_list[playerNum].apply {
-            val cardAdapter=CardSectionAdapter(viewModel.playersCardList.value!![playerNum].cardList,viewModel.cardWidth,viewModel.cardHeight,playerNum)
-            adapter=cardAdapter
-            layoutManager=LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL,false)
+            val cardAdapter = CardSectionAdapter(
+                viewModel.playersList.value!![playerNum].cardList,
+                viewModel.cardWidth,
+                viewModel.cardHeight,
+                playerNum
+            )
+            adapter = cardAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity, RecyclerView.HORIZONTAL, false)
         }
 
     }
-    private fun setSharedCardRecyclerView(playerNum: Int){
-        val gridSpan=when(playerNum){
-            4->{
+
+    private fun setSharedCardRecyclerView(playerNum: Int) {
+        val gridSpan = when (playerNum) {
+            4 -> {
                 4
             }
-            5->{
+
+            5 -> {
                 6
             }
-            else->{
+
+            else -> {
                 5
             }
         }
 
         binding.rvCardShared.apply {
-            val cardAdapter=CardSectionAdapter(viewModel.sharedCardList.value!!,viewModel.cardWidth,viewModel.cardHeight,3)
-            adapter=cardAdapter
-            layoutManager= GridLayoutManager(this@MainActivity,gridSpan)
-            for(i in 0 until itemDecorationCount){
+            val cardAdapter = CardSectionAdapter(
+                viewModel.sharedCardList.value!!,
+                viewModel.cardWidth,
+                viewModel.cardHeight,
+                3
+            )
+            adapter = cardAdapter
+            layoutManager = GridLayoutManager(this@MainActivity, gridSpan)
+            for (i in 0 until itemDecorationCount) {
                 val decoration = getItemDecorationAt(i)
                 if (decoration is SharedCardItemDecorator) {
                     removeItemDecoration(decoration)
@@ -163,41 +189,40 @@ class MainActivity : AppCompatActivity() {
     /**
      * 인원 수에 맞게 card 영역의 visibility를 적절히 초기화한다.
      */
-    private fun initViewByPlayerNum(playerNum:Int){
-        when(playerNum){
-            3->{
-                for(i in 0 until 3){
-                    card_section_rv_list[i].visibility= View.VISIBLE
-                    card_section_tv_list[i].visibility=View.VISIBLE
+    private fun initViewByPlayerNum(playerNum: Int) {
+        when (playerNum) {
+            3 -> {
+                for (i in 0 until 3) {
+                    card_section_rv_list[i].visibility = View.VISIBLE
+                    card_section_tv_list[i].visibility = View.VISIBLE
                 }
 
-                card_section_tv_list[3].visibility=View.INVISIBLE
-                card_section_rv_list[3].visibility=View.INVISIBLE
+                card_section_tv_list[3].visibility = View.INVISIBLE
+                card_section_rv_list[3].visibility = View.INVISIBLE
 
-                card_section_tv_list[4].visibility=View.GONE
-                card_section_rv_list[4].visibility=View.GONE
+                card_section_tv_list[4].visibility = View.GONE
+                card_section_rv_list[4].visibility = View.GONE
             }
 
-            4->{
-                for(i in 0 until 4){
-                    card_section_rv_list[i].visibility=View.VISIBLE
-                    card_section_tv_list[i].visibility=View.VISIBLE
+            4 -> {
+                for (i in 0 until 4) {
+                    card_section_rv_list[i].visibility = View.VISIBLE
+                    card_section_tv_list[i].visibility = View.VISIBLE
                 }
-                card_section_tv_list[4].visibility= View.GONE
-                card_section_rv_list[4].visibility= View.GONE
+                card_section_tv_list[4].visibility = View.GONE
+                card_section_rv_list[4].visibility = View.GONE
             }
 
-            5->{
-                card_section_rv_list.forEach{
-                    it.visibility=View.VISIBLE
+            5 -> {
+                card_section_rv_list.forEach {
+                    it.visibility = View.VISIBLE
                 }
-                card_section_tv_list.forEach{
-                    it.visibility=View.VISIBLE
+                card_section_tv_list.forEach {
+                    it.visibility = View.VISIBLE
                 }
             }
         }
     }
-
 
 
     /**
--- app/src/main/java/hd/softeer/luckycardgame/MainActivityViewModel.kt
+++ app/src/main/java/hd/softeer/luckycardgame/MainActivityViewModel.kt
@@ -6,77 +6,51 @@ import androidx.lifecycle.ViewModel
 import hd.softeer.luckycardgame.model.Animal
 import hd.softeer.luckycardgame.model.Card
 import hd.softeer.luckycardgame.model.CardNumber
+import hd.softeer.luckycardgame.model.GameInfo
 import hd.softeer.luckycardgame.model.User
 
-class MainActivityViewModel: ViewModel() {
-    private val totalCardList: MutableList<Card> = mutableListOf()
+class MainActivityViewModel : ViewModel() {
 
-    private val _playersCardList = MutableLiveData<MutableList<User>>()
-    val playersCardList: LiveData<MutableList<User>> get()=_playersCardList
+    private val gameManager = LuckyGame()
 
-    private val _sharedCardList = MutableLiveData<MutableList<Card>>()
-    val sharedCardList:LiveData<MutableList<Card>> get()=_sharedCardList
+    private val _playersList = MutableLiveData<List<User>>()
+    val playersList: LiveData<List<User>> get() = _playersList
+
+    private val _sharedCardList = MutableLiveData<List<Card>>()
+    val sharedCardList: LiveData<List<Card>> get() = _sharedCardList
 
 
     private var _cardWidth = 0
-    val cardWidth get()=_cardWidth
+    val cardWidth get() = _cardWidth
 
-    private var _cardHeight=0
-    val cardHeight get()=_cardHeight
+    private var _cardHeight = 0
+    val cardHeight get() = _cardHeight
 
-    init{
-        _playersCardList.value= mutableListOf()
-        _sharedCardList.value= mutableListOf()
+    init {
+        _playersList.value = mutableListOf()
+        _sharedCardList.value = mutableListOf()
     }
 
-    fun initCardList(playerNum: Int) {
-        totalCardList.clear()
-        makeCardRandom(getMaxCardNum(playerNum))
-        divideCardToPlayers(playerNum)
-    }
-
-    fun setCardWidth(scale:Float,parentPixelWidth:Int){
-        val parentDpWidth = (parentPixelWidth/ scale + 0.5f).toInt() // Convert pixels to dp
-        val cardDpWidth=parentDpWidth/6-8
+    fun setCardWidth(scale: Float, parentPixelWidth: Int) {
+        val parentDpWidth = (parentPixelWidth / scale + 0.5f).toInt() // Convert pixels to dp
+        val cardDpWidth = parentDpWidth / 6 - 8
         val cardPixelWidth = (cardDpWidth * scale + 0.5f).toInt() // Convert dp to pixels
 
-        _cardWidth=cardPixelWidth
+        _cardWidth = cardPixelWidth
     }
-    fun setCardHeight(scale:Float,parentPixelHeight:Int){
-        val parentDpHeight = (parentPixelHeight/ scale + 0.5f).toInt()
-        val cardDpHeight=parentDpHeight-16
+
+    fun setCardHeight(scale: Float, parentPixelHeight: Int) {
+        val parentDpHeight = (parentPixelHeight / scale + 0.5f).toInt()
+        val cardDpHeight = parentDpHeight - 16
         val cardPixelHeight = (cardDpHeight * scale + 0.5f).toInt()
 
-        _cardHeight=cardPixelHeight
+        _cardHeight = cardPixelHeight
     }
 
-    private fun makeCardRandom(cardMaxNumber: Int) {
-        for (type in Animal.values()) {
-            for (number in 1..cardMaxNumber) {
-                totalCardList.add(Card(type, CardNumber(number)))
-            }
-        }
-        totalCardList.shuffle()
+    fun startGame(playerNumber: Int) {
+        gameManager.initGame(playerNumber)
+        _playersList.value = gameManager.getPlayersCardInfo()
+        _sharedCardList.value = gameManager.getSharedCardInfo()
     }
 
-    private fun divideCardToPlayers(playerNum:Int){
-        val cardsNumPerPlayer=11-playerNum
-        _playersCardList.value!!.clear()
-        _sharedCardList.value!!.clear()
-
-        var start=0
-        var end=cardsNumPerPlayer-1
-        for(i in 0 until playerNum){
-            _playersCardList.value!!.add(User((totalCardList.slice(start..end))))
-            start+=cardsNumPerPlayer
-            end+=cardsNumPerPlayer
-        }
-
-        _sharedCardList.value!!.addAll(totalCardList.slice(start..totalCardList.lastIndex))
-
-    }
-
-    private fun getMaxCardNum(playerNum: Int): Int {
-        return if (playerNum == 3) 11 else 12
-    }
 }
